be13764bb87ea271b8c432519c36f25b2fd59d28
sky <sbd0924@gmail.com>
2021-10-20 16:13:57 +0900

    projects

--- firstProject/sngyBab3/src/app/home/home.page.ts
+++ firstProject/sngyBab3/src/app/home/home.page.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { DataService, Message } from '../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  constructor(private data: DataService) {}
+  constructor(private data: DataService, private router: Router) {}
 
   refresh(ev) {
     setTimeout(() => {
@@ -18,5 +20,10 @@ export class HomePage {
   getMessages(): Message[] {
     return this.data.getMessages();
   }
+
+  goLogin(){
+    this.router.navigate(['/login'])
+  }
+
 
 }
